3b9c2f8cfebd0aa508747ad5e1eec23f3cfa6e4a
Michał Budzyński <michal@virtualdesign.pl>
2017-11-11 01:17:23 +0100

    :busts_in_silhouette: rollup config

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
+export default {
+  entry: 'src/main.js',
+  format: 'iife',
+  moduleName: 'greedy-cubes',
+  plugins: [
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    }),
+    serve({
+      contentBase: ['dist']
+    }),
+    livereload({
+      watch: ['dist']
+    })
+  ],
+  dest: 'dist/index.js'
+}
