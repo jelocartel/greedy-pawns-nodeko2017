d1a3bb0a8f446c6f95684eaf08b42f65e6693dfd
Michał Budzyński <michal@virtualdesign.pl>
2017-11-11 02:53:22 +0100

    :loudspeaker: movable component on player

--- src/player.js
+++ src/player.js
@@ -1,20 +1,33 @@
 import { CONFIG } from './config';
 
-import { Render, Transform } from 'cervus/components';
+import { Render, Transform, Move } from 'cervus/components';
 import { Box } from 'cervus/shapes';
 
 export class Player {
   constructor() {
+    this.lastX = 0;
+    this.lastY = 0;
+    this.last_colored = '';
 
     this.entity = new Box();
+    this.entity.add_component(new Move({
+      keyboard_controlled: true,
+      rotate_speed: 0
+    }));
+
     this.components = {
       transform: this.entity.get_component(Transform),
-      render: this.entity.get_component(Render)
+      render: this.entity.get_component(Render),
+      move: this.entity.get_component(Move)
     };
 
     this.components.render.material = CONFIG.material;
     this.components.render.color = 'ff00ff';
     this.components.transform.position = [0, 0, 3];
     CONFIG.game.add(this.entity);
+  }
+
+  on_tick() {
+
   }
 }
